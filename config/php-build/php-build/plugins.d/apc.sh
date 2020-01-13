774d2ff7dbe95da0def1096ab056cddab17d60a6
katakatataan <yuki.matsuyama0123@gmail.com>
2020-01-13 22:43:45 +0900

    delete extra

--- config/php-build/php-build/plugins.d/apc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash 	
-# 	
-# This shell scriplet is meant to be included by other shell scripts 	
-# to set up some variables and a few helper shell functions.
-
-function install_apc {
-    install_extension "apc" "$1"
-}
